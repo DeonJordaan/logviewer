21a2ddedfb5f37b0c51f6c52e9aec557dcd11854
DeonJ76 <deon.jordaan76@gmail.com>
2021-11-18 17:54:48 +0200

    Converting project from JavaScript to TypeScript - worked on App and useFetch

---
+++ src/store/useFetch.tsx
@@ -0,0 +1,37 @@
+import { useState, useCallback } from 'react';
+
+const useFetch = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState<string | null>(null);
+
+	const sendRequest = useCallback(async (requestConfig, applyData) => {
+		setIsLoading(true);
+		setError(null);
+		try {
+			const response = await fetch(requestConfig.url, {
+				method: requestConfig.method ? requestConfig.method : 'GET',
+				headers: requestConfig.headers ? requestConfig.headers : {},
+				body: requestConfig.body
+					? JSON.stringify(requestConfig.body)
+					: null,
+			});
+
+			if (!response.ok) {
+				throw new Error('Could not retrieve data');
+			}
+
+			const data = await response.json();
+
+			applyData(data);
+		} catch (error: any) {
+			if (error instanceof Error) {
+				setError(error.message);
+			}
+		}
+		setIsLoading(false);
+	}, []);
+
+	return { isLoading, error, sendRequest }; //REMOVED , data
+};
+
+export default useFetch;
