7be6283ba0dfa3cae923729edffa0fe08a648f3d
DeonJ76 <deon.jordaan76@gmail.com>
2021-11-30 21:15:18 +0200

    Experimenting with Getter to set status

--- src/Interfaces/task.ts
+++ src/Interfaces/task.ts
@@ -12,6 +12,17 @@ class Task {
 	message: string;
 	status: number;
 
+	get eventStatus() {
+		const status: string = {
+			0: 'NotSet',
+			1: 'Started',
+			2: 'Completed',
+			3: 'Aborted',
+			4: 'Failed',
+		};
+		return status[statusCode];
+	}
+
 	constructor(data: DataInterface) {
 		this.key = data.Id;
 		this.id = data.Id;
